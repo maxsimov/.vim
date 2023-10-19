44acafef680f7e60a8dc288c8123fa82815edb68
Andrey Maximov <andrey@maxsimov.com>
2023-10-19 18:50:06 +0000

    added dependencies

---
+++ deps.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+apt install pip
+apt install universal-ctags
+apt install fzf
+pip install openai
+pip install jedi
+pip install black
+pip install autoflake
+pip install autoimport
+pip install click
+pip install fail2ban
+pip install flake8
+pip install flake8-black
+pip install ImageHash
+pip install jsonschema
+pip install mutagen
+pip install mypy
+pip install PyExifTool
+pip install ruff
+pip install SQLAlchemy
+pip install videohash
+pip install asciimatics
